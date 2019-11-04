10125130b0f62de39c97f0d19d4e201fabfa901e
oussou-dev <oussou.dev@gmail.com>
2019-11-04 17:52:13 +0000

    Add CRUD routes in server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,11 +8,35 @@ contact_list=[{"nom":"Einstein","prenom":"Albert","tel":"06.00.00.00.00","mail":
               {"nom":"Fourier","prenom":"Joseph","tel":"09.00.03.00.01","mail":"joseph@maserie.fr"}
               ]
 
+
+@app.route('/')
 def index():
     return render_template("index.html",contact_list=contact_list)
 
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == "GET":
+        return render_template('create.html', contact=None)
+    
+    if request.method == "POST":
+        contact_list.append(request.form.todict())
+        return redirect(url_for('index'))
+        
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    if request.methd == 'GET':
+        return contact = contact_list[id-1]
+    
+    if request.method == 'POST':
+        contact_list[id-1] = request.form.todict()
+        return redirect(url_for('index'))
+        
+@app.route('/delete/<int:id>', methodes=['GET', 'POST'])
+def delete(id):
+    if request.method == 'GET':
+        return render_template('delete.html', contact = contact_list[id-1])
+        
+    if request.method =='POST':
+        del contact[id-1]
+        return redirect (url_for('index'))
